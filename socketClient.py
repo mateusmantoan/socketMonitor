bc6631f68710b9dae626b74ddca556bd698be13e
mateusmantoan <mateusmantoan@gmail.com>
2019-10-27 13:29:24 -0300

    Create socketClient.py

---
+++ socketClient.py
@@ -0,0 +1,25 @@
+import socket
+import commands
+import time
+
+print('IP Server: ')
+HOST = raw_input()
+PORT = 2907
+print('Frequency of monitoring (in seconds): ')
+FREQUENCY = raw_input()
+
+while True:
+    mem = commands.getoutput('cat /proc/meminfo | grep Mem')
+    disc = commands.getoutput('df --total -h | grep total')
+    top = commands.getoutput("top -n 1 | sed -n '2,5p'")
+
+    msg = '\n\nMEMORY INFORMATIONS:\n' + mem + '\n\nACTUAL STATE OF STORAGE DISC\n' + disc + '\n\nTOP CMD OUTPUT:\n' + top + '\n\n\n'
+    msg_disc = 'ACTUAL STATE OF STORAGE DISC'
+    msg_top = 'TOP CMD OUTPUT:'
+    msg_users = 'USERS CURRENTLY LOGGED'
+
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    dest = (HOST, PORT)
+    udp.sendto (msg, dest)
+    udp.close()
+    time.sleep(int(FREQUENCY))
